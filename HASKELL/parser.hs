b74849322b653af0777647f75dd9202f0ff5e80f
Oz-otR <32718559+Oz-otR@users.noreply.github.com>
2018-02-19 10:36:36 -0700

    newer
    close

--- HASKELL/parser.hs
+++ HASKELL/parser.hs
@@ -3,15 +3,19 @@ import System.Environment
 import System.Exit
 import System.IO
 import Control.Exception
+import Data.IORef
+import Data.List
+import Data.Maybe
+
+type Counter = Int -> IO Int
 
 sections = ["Name:"
             ,"forced partial assignment:"
-            ,"forbidden machine:","too-near tasks:"
+            ,"forbidden machine:"
+	    ,"too-near tasks:"
             ,"machine penalties:"
             ,"too-near penalities:"
             ,"\n"]
-
-penalty = "1 1 1 1 1 1 1 1\n"
 
 -- strtoA :: String -> Array
 -- strtoA x = listArray(0, length x - 1) x
@@ -29,7 +33,13 @@ eqs4 = do putStrLn "What is 2 + 2?"
               then putStrLn "You're right!"
               else putStrLn "You're wrong!"
 
-main = handle handler $ do
+main = turn
+
+turn = handle handler $ do 
+--    counter <- mkCntr
+--    update counter
+--    errPrnt (get counter)
+
     args <- getArgs
     if length args /= 2
        then do 
@@ -40,10 +50,57 @@ main = handle handler $ do
               	x <- try $ readFile file
 		case x of
 		  Left exc -> handler exc
-		  Right content -> Process content
+		  Right content -> print $ lines content
+				-- print $ getIn 0 $ lines content
+		  		-- print $ lines content -- lines in a list
+		    		-- llst content
+		  		-- writeout (args !! 1) 0
+		  		-- writeFile (args !! 1)  content
+		  		-- putStr content (writes to stdIO)
 
 handler :: IOError -> IO ()
 handler ex = putStrLn (errPrnt 0) >> exitFailure
+
+-- Does some dumb shit ignore its a test
+llst cnt = do
+     let l = map ((!!) (lines cnt)) [0,1..(length (lines cnt) - 1)]
+     let line = l !! 0
+     putStr line
+     return line
+
+-- adWrite = 1:(2:(3:(4:(5:[]))))
+-- gets a list of indicies as to where the sections are
+getIn x list = if x < 6
+      	       	  then (fromMaybe (-1) $ elemIndex (sections !! x) list):getIn (x + 1) list
+		  else []
+
+
+-- Global Counter >> IMPURE: dont know if ill need it
+mkCntr :: IO Counter
+mkCntr = do
+    r <- newIORef 0
+    return (\i -> do modifyIORef r (+i)
+                     readIORef r)
+		     
+update :: Counter -> IO ()
+update counter = do
+    a <- counter 1
+    print [a]
+
+get :: Counter -> IO ()
+get counter = do
+    a <- counter 0
+    print a
+
+-- Does some dumb shit ignore its a test
+llst cnt = do
+     let l = map ((!!) (lines cnt)) [0,1..(length (lines cnt) - 1)]
+     let line = l !! 0
+     putStr line
+     return line
+
+-- writeout :: Int ->
+writeout path x = writeFile path (errPrnt x)
 
 errPrnt :: Int -> String
 errPrnt 0 = "File Not Found"
