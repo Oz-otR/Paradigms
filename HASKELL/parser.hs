ee73c42d1a6c32f0d8d214b78a53b1a6d04f849d
sarahhatcher <sarah.hatchersmallma@ucalgary.ca>
2018-03-16 19:59:21 -0600

    In progress data structure

--- HASKELL/parser.hs
+++ HASKELL/parser.hs
@@ -6,17 +6,32 @@ import Data.IORef
 import Data.List
 import Data.Maybe
 import Data.Char
+import Text.Read
 
 -- section Names?
 sections = ["Name:"
             ,"forced partial assignment:"
             ,"forbidden machine:"
-	    ,"too-near tasks:"
+	          ,"too-near tasks:"
             ,"machine penalties:"
             ,"too-near penalities"
             ,"\n"]
 
-main = turn
+main = putStrLn "Before turn" >> turn
+
+turn :: IO()
+turn = handle handler $ do
+    args <- getArgs
+    if length args /= 2
+       then do
+       	    putStrLn $ errPrnt 1
+	    exitFailure
+       else case init args of
+              [file] -> do
+              	x <- try $ readFile file
+		case x of
+		  Left exc -> handler exc
+		  Right content -> pass (args !! 1) content
 
 {-
 declare a catch
@@ -26,28 +41,39 @@ else try to read file
 if file doesnt exist print out File not found
 else read all contents, and pass on both the outstream & content for parsing
 -}
-turn :: IO()
-turn = handle handler $ do 
+
+{-turn :: IO()
+turn = handle handler $ do
+    --putStrLn "Entering Turn"
     args <- getArgs
     if length args /= 2
-       then do 
+       then do
        	    putStrLn $ errPrnt 1
-	    exitFailure
-       else case init args of 
+	          exitFailure
+       else case init args of
               [file] -> do
               	x <- try $ readFile file
-		case x of
-		  Left exc -> handler exc
-		  Right content -> pass (args !! 1) content
+            		case x of
+            		  Left exc -> handler exc
+                  --Right content -> (putStrLn "Hello") >> pass (args !! 1) content
+                  Right content -> pass (args !! 1) content--}
+
 
 handler :: IOError -> IO ()
-handler ex = putStrLn (errPrnt 0) >> exitFailure
+hmapToInt :: [String] -> [Int]
+mapToInt l = map read l
+
+mapFullToInt :: [[String]] -> [[Int]]
+mapFullToInt l = do
+  w <- l
+  return $ mapToInt w
+andler ex = putStrLn (errPrnt 0) >> exitFailure
 
 writeout :: FilePath -> Int -> IO ()
 writeout path x = writeFile path (errPrnt x)
 
 {-
-Check if theres at least one \n between each sections. 
+Check if theres at least one \n between each sections.
 Filters out return carriage for in Windows Based Files
 prints parse error and exits if false
 prunes all empty strings, lines that are just the newline char
@@ -58,15 +84,18 @@ send the list of sections and the out stream to parsedriver to be parsed
 -}
 pass :: p -> [Char] -> IO ()
 pass out content = do
-     { if not(leastone (filter (/= '\r') content) 1)
+     {
+     if not(leastone (filter (/= '\r') content) 1)
           then putStrLn (errPrnt 10) >> exitFailure
 	  else return()
+     ; putStrLn "Starting pruning"
      ; let pruned = removeEmpties $ lines $ filter (/= '\r') content
      ; let index = getIn 0 pruned
      ; if not(sorted index && not ((-1) `elem` index))
        	  then putStrLn (errPrnt 10) >> exitFailure
 	  else return ()
      ; let blocks = reverse $ splt 5 pruned index
+     ; putStrLn "In Final"
      ; parseDriver out blocks -- print blocks to view the list
      }
 
@@ -80,10 +109,31 @@ parseDriver out content = do
      ; pfpa out $ content !! 1
      ; pfm out $ content !! 2
      ; ptnt out $ content !! 3
-     ; pmp out $ content !! 4
+     ; let splitByWords = mapByWord $ content !! 4
+     ; let (_:end) = splitByWords
+     ; let listO'ListO'IntsOH = mapFullToInt end
+    -- ; map (\n -> if n >= 0 then ( putStrLn (errPrnt 6) >> exitFailure) else return ()) listO'ListO'IntsOH
+    ; flattenListIO . checkListNegs $ listO'ListO'IntsOH
+     -- ; pmp out $ content !! 4
      ; ptnp out $ content !! 5
-     -- ; print $ content
+     --; print $ content
+     ; print listO'ListO'IntsOH
      }
+
+checkNegs :: [Int] -> IO ()
+checkNegs [] = return ()
+checkNegs (n:ns) = if n < 0 then (putStrLn (errPrnt 6) >> exitFailure) else checkNegs ns
+
+{-checkListNegs :: [[Int]] -> IO ()
+checkListNegs [] = return ()
+checkListNegs (ns:nss) = if checkNegs ns == () then checkListNegs nss else (putStrLn (errPrnt 6) >> exitFailure)-}
+
+checkListNegs :: [[Int]] -> [IO ()]
+checkListNegs = map checkNegs 
+
+flattenListIO :: [IO ()] -> IO ()
+flattenListIO [] = return ()
+flattenListIO (io:ios) = io >> flattenListIO ios
 
 {-
 checks if theres more than one name or if theres 2 names separated by a ' '
@@ -110,7 +160,7 @@ pfpa out l = if ((length l) == 1)
 		--writeout out 3 >> exitFailure
 
 {-
-Exactly the Same as Above 
+Exactly the Same as Above
 -}
 pfm :: p -> [[Char]] -> IO ()
 pfm out l = if ((length l) == 1)
@@ -141,7 +191,7 @@ invalid penalty or machine penalty error
 
 -- check number of occurences of ' '
 -- check length $ words line
--- 
+--
 pmp :: p -> [[Char]] -> IO ()
 pmp out l = if ((length l) /= 9)
            then putStrLn (errPrnt 4) >> exitFailure
@@ -149,7 +199,13 @@ pmp out l = if ((length l) /= 9)
 	       then return()
 	       else putStrLn (errPrnt $ mprc l 1) >> exitFailure
 	       --writeout out (mprc l 1) >> exitFailure
+mapToInt :: [String] -> [Int]
+mapToInt l = map read l
 
+mapFullToInt :: [[String]] -> [[Int]]
+mapFullToInt l = do
+  w <- l
+  return $ mapToInt w
 {-
 if section only contains header return
 else recursively check using TNP flag
@@ -183,7 +239,7 @@ cehcks for length and negatives
 returns an int to be checked or used as an arguments for errprint
 -}
 
--- instead of renon num check for maybe int > 0 
+-- instead of renon num check for maybe int > 0
 mprc :: Num p => [[Char]] -> Int -> p
 mprc args index = if index > 8
      	  	     then 0
@@ -210,7 +266,24 @@ patt ('(':a:',':b:')':xs)
     | ((length xs) == 0 && a `elem` ['1'..'8'] && b `elem`['A'..'H'])= "FF"
     | ((length xs) == 0 && a `elem` ['A'..'H'] && b `elem`['A'..'H'])= "TNT"
     | otherwise = "No"
+patt ('(':a:',':b:',':xs)
+	| a `elem` ['A'..'H'] && b `elem` ['A'..'H'] = potentialTNP xs
+	| otherwise = "No"
 patt other = "No"
+
+potentialTNP xs = checkClosed . reverse $ xs
+checkClosed (')':xs) = checkCanAlpha . maybeCanAlpha $ xs
+checkClosed other = "No"
+
+checkCanAlpha :: Maybe Int -> String
+checkCanAlpha (Just(_)) = "TNP"
+checkCanAlpha (Nothing) = "No"
+
+maybeCanAlpha :: String -> Maybe Int
+maybeCanAlpha = readMaybe . map (repl 'A' ' ')
+
+repl :: Char -> Char -> Char -> Char
+repl c' c s = if s == c then c' else s
 
 -- checks if args are in right order
 sorted :: (Ord a) => [a] -> Bool
