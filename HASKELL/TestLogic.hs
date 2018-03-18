4b491120f6d04a667d526b6efada4b46c6b4c065
smithcg1 <32621429+smithcg1@users.noreply.github.com>
2018-03-17 18:50:12 -0600

    Add files via upload

--- HASKELL/TestLogic.hs
+++ HASKELL/TestLogic.hs
@@ -1,59 +1,56 @@
 import Data.Char
 import Data.List
 
-main = putStrLn . show $ parseNode testlist currentPath testTNP 0
+main = putStrLn . show $ (parseNode testlist testTNP currentPath 0)  --Prints lowest penalty of search
 
-testlist = [[0,2,-1],
-            [-1,1,0],
-            [1,0,-1]]
+testlist = [[1,1,2],
+            [1,1,1],
+            [1,1,2]]
 
-testTNP =   [[0,0,-1],
-             [0,0,9],
+testTNP =   [[0,0,0],
+             [0,0,0],
              [0,0,0]]
 
-currentPath = [0,0,0]
-bestPath = [[0,0,0],[0]]
+currentPath = []
+bestPath = [[0,0,0],[0]] -- Delete this line
+worstPath = [[0,1,2],[500]]
 
 
 -- Finds biggest number in a 2-D list
-parseNode :: [[Int]] -> [[Int]] -> [Int] -> Int -> [Int]
+parseNode :: [[Int]] -> [[Int]] -> [Int] -> Int -> [[Int]]
 parseNode list tnplist currentPath totalPen
     | taillist /= [] =
-        myMin [parseNode (safeAdd2DArr (editCols (-1) index taillist) (tnplist !! index)) tnplist (currentPathSet index) (totalPen + (taskPenalty index)) | index <- [0..2], (taskPenalty index) /= -1]
-    | otherwise = parseLeaf headlist totalPen
-    where   newTaillist n = safeAdd2DArr taillist n  --adds TNP to head of tail list
-            taskPenalty n = (headlist !! n)
+        returnLowPen [parseNode (safeAdd2DLst (editCols (-1) index taillist) (tnplist !! index)) tnplist  (addToPath index) (totalPen + (headlist !! index)) | index <- [0..2], (headlist !! index) /= -1]
+    | otherwise = returnLowPen [assembleLeaf (addToPath index) (addFirstTNP index) | index <- [0..2], (headlist !! index) /= -1]
+    where   newTaillist n = safeAdd2DLst taillist n  --adds TNP to head of tail list
             headlist:taillist = list
-            currentPathSet n m = currentPath !!  --Could choose   "do (currentPath !! length m) = n    *takes index list
-
---Or, pass [[0,0,0],[0,0,0],[0]] down the recursion chain and parse them on the way up
-
-parseLeaf :: [Int] -> [Int] -> Int -> Int
-parseLeaf list currentPath totalPen  = [parseNode currentPath (totalPen + (taskPenalty index)) | index <- [0..2], (taskPenalty index) /= -1] --penalty stuff
-    where   taskPenalty n = (list !! n)
+            addToPath n = n:currentPath
+            addFirstTNP n = (totalPen + (safeAdd1DLst headlist (tnplist !! (currentPath !! 0))) !! n)
 
 
-myMin $ map (+totalPen) $ filter (/= -1) list 
-    where headlist:remaininglist = list
+--Assembles the leaf information into [[path][pen]]
+assembleLeaf :: [Int] -> Int -> [[Int]]
+assembleLeaf currentPath totalPen  = (currentPath:[[totalPen]])
 
 
+returnLowPen :: [[[Int]]] -> [[Int]]
+returnLowPen list = foldl(\acc leaf -> if (acc !! 1) < (leaf !! 1) then acc else leaf) worstPath list   --acc is a single 2D array leaf information packet [[path][pen]]
 
---Takes a 2D list and adds a 1D list to it
-safeAdd2DArr :: [[Int]] -> [Int] -> [[Int]]
-safeAdd2DArr penlist tnplist = (safeAdd1DArr x tnplist):xs
+
+--Takes a 2D list and adds a 1D list to it except for -1s
+safeAdd2DLst :: [[Int]] -> [Int] -> [[Int]]
+safeAdd2DLst penlist tnplist = (safeAdd1DLst x tnplist):xs
     where x:xs = penlist
 
-safeAdd1DArr :: [Int] -> [Int] -> [Int]
-safeAdd1DArr = zipWith $ \a b -> if a == -1 || b == -1  then -1 else a + b
+
+safeAdd1DLst :: [Int] -> [Int] -> [Int]
+safeAdd1DLst = zipWith $ \a b -> if a == -1 || b == -1  then -1 else a + b
 
 
+--Changes the value for an entire column of a 2d array to selected value
 editCol :: Int -> Int -> [Int] -> [Int]
 editCol n 0 (x:xs) = n:xs
 editCol n i (x:xs) = x:editCol n (i - 1) xs
 
 editCols :: Int -> Int -> [[Int]] -> [[Int]]
 editCols n i = map (editCol n i)
-
-myMin [] = maxBound :: Int
-myMin xs = minimum xs  
-
