471c043f463e9d3158fa0376faa3714d11032075
smithcg1 <32621429+smithcg1@users.noreply.github.com>
2018-04-04 23:06:29 -0600

    All items asserted
    All sections are checked and asserted if acceptable. Error message are NOT accurate to the type of error.

--- PROLOG/parser.pl
+++ PROLOG/parser.pl
@@ -1,4 +1,7 @@
 %treeNode(Task, Machine, Penalty) will be added during runtime
+%fpaNode(Task, Machine) will be added during runtime
+%forbidMNode(Task, Machine) will be added during runtime
+%tntNode(Task, Task) will be added during runtime
 
 main :-
     %Get the arguments from command line
@@ -9,17 +12,46 @@ main :-
     open(InputFile, read, Str), %this really doesn't need a comment to explain
     read_file(Str,Lines),
     close(Str),
-    write(Lines), nl,
+%    write(Lines), nl,
     maplist(removeLastSpaces(), Lines, SpacelessLines),
-    assert(treeNode(-1,-1,-1)), %Creates first treeNode to allow others to be added
+    generatePredSeeds,
     validInput(SpacelessLines, Out),
-    retract(treeNode(-1,-1,-1)),
-    write(Out), nl,
-    printTreeNodes.
+    destroyPredSeeds,
+%    write(Out), nl,
+    printAll.
+
+	
+	
+%Creates an invalid version assignment of the predicates so inital checks dont crash
+%(they are removed later)
+generatePredSeeds() :-
+    assert(fpaNode(-1,-1)),
+    assert(forbidMNode(-1,-1)),
+	assert(tntNode(-1,-1)),
+	assert(treeNode(-1,-1,-1)),
+	assert(tnpNode(-1,-1,-1)).
+
+%Destroys predicate seeds
+destroyPredSeeds() :-
+    retract(fpaNode(-1,-1)),
+    retract(forbidMNode(-1,-1)),
+	retract(tntNode(-1,-1)),
+	retract(treeNode(-1,-1,-1)),
+	retract(tnpNode(-1,-1,-1)).
 
 
-%Prints content of all current "treeNode" predicates for
-printTreeNodes() :- treeNode(Task, Machine, Penalty), write((Task, Machine, Penalty)), nl, fail; true.
+
+
+%Debug print statments-------------------------------------
+printAll() :- printFPA, printForbidM, printTNT, printTreeNodes, printTNP, nl,nl.
+printFPA() :- write("---fpa---"), nl, fpaNode(Task, Machine), write((Task, Machine)), nl, fail; true.
+printForbidM() :- nl, write("---forbid M---"), nl,forbidMNode(Task, Machine), write((Task, Machine)), nl, fail; true.
+printTNT() :- nl, write("---tnt---"), nl,tntNode(Task1, Task2), write((Task1, Task2)), nl, fail; true.
+printTreeNodes() :- nl, write("---Tree---"), nl,treeNode(Task, Machine, Penalty), write((Task, Machine, Penalty)), nl, fail; true.
+printTNP() :- nl, write("---tnp---"), nl,tnpNode(Task, Machine, Penalty), write((Task, Machine, Penalty)), nl, fail; true.
+%----------------------------------------------------------
+
+
 
 
 %Put all of the file into a list
@@ -33,76 +65,200 @@ read_file(Stream,[X|L]) :-
     atom_chars(X, Codes),
     read_file(Stream,L), !.
 
-%DCG for parsing
+
+
+
+%-----------------DCG for parsing--------------------
 validInput --> name,fpa,forbidM,tnt,machs,tnp.
 
 %DCG for parsing Name information
 name --> nameHeader, nameBody,arbLines.
 
 nameHeader --> ['Name:'].
-nameBody --> [X], {hasInternalSpace(X)}. %Checks if name is of valid form (no spaces)
+nameBody --> [X], {hasInternalSpace(X)}.
 
-%DCG for parsing Forced Partial Assignment information 
-fpa-->fpaHeader,fpaBody,arbLines.
 
-fpaHeader--> ['forced partial assignment:'].
-fpaBody--> [].
+%DCG for parsing Forced Partial Assignment information
+fpa --> fpaHeader,fpaBody('0').
 
+fpaHeader --> ['forced partial assignment:'].
+fpaBody(Flag) --> fpaEatPair(Flag, NewFlag), {NewFlag = '0'; NewFlag = '1'}, fpaBody(NewFlag) ;
+	{NewFlag = '2', Flag = '1'}, [];
+	{NewFlag = '2', Flag = '0'}, arbLines.
+	
+fpaEatPair(Flag, NewFlag) --> 
+	[X], {parseFPA(X), NewFlag = '0'}; 
+	[''], {NewFlag = '1'};
+	[], {NewFlag = '2'}.
+
+
+	
 %DCG for parsing Forbidden Machine information
-forbidM-->forbidHeader,forbidBody,arbLines.
+forbidM --> forbidHeader,forbidBody('0').
 
-forbidHeader--> ['forbidden machine:'].
-forbidBody-->[].
+forbidHeader --> ['forbidden machine:'].
+forbidBody(Flag) --> forbidEatPair(Flag, NewFlag), 
+	{NewFlag = '0'; NewFlag = '1'}, forbidBody(NewFlag) ;
+	{NewFlag = '2', Flag = '1'}, [];
+	{NewFlag = '2', Flag = '0'}, arbLines.
+	
+forbidEatPair(Flag, NewFlag) --> 
+	[X], {parseForbid(X), NewFlag = '0'}; 
+	[''], {NewFlag = '1'};
+	[], {NewFlag = '2'}.
+
+	
+	
+%DCG for parsing Too-near Penalty information
+tnt --> tntHeader,tntBody('0').
+
+tntHeader --> ['too-near tasks:'].
+tntBody(Flag) --> tntEatPair(Flag, NewFlag), 
+	{NewFlag = '0'; NewFlag = '1'}, tntBody(NewFlag) ;
+	{NewFlag = '2', Flag = '1'}, [];
+	{NewFlag = '2', Flag = '0'}, arbLines.
+	
+tntEatPair(Flag, NewFlag) --> 
+	[X], {parseTNT(X), NewFlag = '0'}; 
+	[''], {NewFlag = '1'};
+	[], {NewFlag = '2'}.
+
+
+	
+%DCG for parsing Machine Penalty information
+machs --> machsHeader,machsBody,arbLines.
+
+machsHeader --> ['machine penalties:'].
+machsBody --> mN,mN,mN,mN,mN,mN,mN,mN.
+mN --> [X], {parseArray(X)}.
+
+
 
 %DCG for parsing Too-near Penalty information
-tnt-->tntHeader,tntBody,arbLines.
+tnp --> tnpHeader,tnpBody('0').
 
-tntHeader--> ['too-near tasks:'].
-tntBody-->[].
+tnpHeader --> ['too-near penalities'].
+tnpBody(Flag) --> tnpEatPair(Flag, NewFlag), 
+	{NewFlag = '0'; NewFlag = '1'}, tnpBody(NewFlag) ;
+	{NewFlag = '2', Flag = '1'}, [];
+	{NewFlag = '2', Flag = '0'}, after.
+	
+tnpEatPair(Flag, NewFlag) --> 
+	[X], {parseTNP(X), NewFlag = '0'}; 
+	[''], {NewFlag = '1'};
+	[], {NewFlag = '2'}.
 
-%DCG for parsing Machine Penalty information
-machs-->machsHeader,machsBody,arbLines.
 
-machsHeader--> ['machine penalties:'].
-machsBody-->mN,mN,mN,mN,mN,mN,mN,mN.
-mN --> [X], {parseArray(X)}.  
 
+%DCG for blank Newlines
+arbLines --> [''],after.
+after --> [''],after.
+after --> [].
+
+
+
+
+
+%Confirms an assignment FPA pair ex:(1,A) is of correct form.
+parseFPA(Atom) :-
+    atom_codes(Atom, List),
+    List = [OB,INT,COMMA,CHAR,CB|Remainder],
+    OB =:= "(",
+    INT >= "1", INT =< "7",
+    COMMA =:= ",",
+    CHAR >= "A", CHAR =< "H",
+    CB =:= ")",
+    Remainder = [],
+    char_code(Task, INT), char_code(Mach, CHAR),
+    assert(fpaNode(Task,Mach));         %Can change to  "assert(fpaNode(INT,CHAR));" if you want it in # not atom form
+	\+ Atom = '', 
+	\+ Atom = 'forbidden machine:', 
+	write('Error with fpa format for pairing |'), write(Atom), write('|'), nl,halt(0).
+
+	
+	
+	
+%Confirms an assignment FPA pair ex:(1,A) is of correct form.
+parseForbid(Atom) :-
+    atom_codes(Atom, List),
+    List = [OB,INT,COMMA,CHAR,CB|Remainder],
+    OB =:= "(",
+    INT >= "1", INT =< "7",
+    COMMA =:= ",",
+    CHAR >= "A", CHAR =< "H",
+    CB =:= ")",
+    Remainder = [],
+    char_code(Task, INT), char_code(Mach, CHAR),
+    assert(forbidMNode(Task,Mach));         %Can change to  "assert(fpaNode(INT,CHAR));" if you want it in # not atom form
+	\+ Atom = '', 
+	\+ Atom = 'too-near tasks:', 
+	write('Error with forbid format for pairing |'), write(Atom), write('|'), nl,halt(0).
+
+
+%Confirms an assignment FPA pair ex:(1,A) is of correct form.
+parseTNT(Atom) :-
+    atom_codes(Atom, List),
+    List = [OB,INT1,COMMA,INT2,CB|Remainder],
+    OB =:= "(",
+    INT1 >= "1", INT1 =< "7",
+    COMMA =:= ",",
+    INT2 >= "1", INT2 =< "7",
+    CB =:= ")",
+    Remainder = [],
+    char_code(Task1, INT1), char_code(Task2, INT2),
+    assert(tntNode(Task1,Task2));         %Can change to  "assert(fpaNode(INT,CHAR));" if you want it in # not atom form
+	\+ Atom = '', 
+	\+ Atom = 'machine penalties:', 
+	write('Error with tnt format for pairing |'), write(Atom), write('|'), nl,halt(0).
+	
+	
+%Confirms an assignment FPA pair ex:(1,A) is of correct form.
+parseTNP(Atom) :-
+    atom_codes(Atom, List),
+    List = [OB,INT1,COMMA,CHAR,COMMA,INT2,CB|Remainder],
+    OB =:= "(",
+    INT1 >= "1", INT1 =< "7",
+    COMMA =:= ",",
+    CHAR >= "A", INT2 =< "H",
+	INT2 >= "1", INT1 =< "9",				%Needs to accept any int, not 1<X<9
+    CB =:= ")",
+    Remainder = [],
+    char_code(Task, INT1), char_code(Mach, CHAR), char_code(Penalty, INT2),
+    assert(tnpNode(Task,Mach,Penalty));         %Can change to  "assert(fpaNode(INT,CHAR));" if you want it in # not atom form
+	\+ Atom = '', 
+	write('Error with tnt format for pairing |'), write(Atom), write('|'), nl,halt(0).
+	
+	
+	
+	
 %Confirms array is of correct form, all nums >= 0, and asserts the results
-parseArray(SpaceList):- 
-    split_string(SpaceList,' ','',NoSpaceList),
-    length(NoSpaceList,8),
-    maplist(atom_number(),NoSpaceList,IntList),
+parseArray(Atom):-
+    split_string(Atom,' ','',AtomList),
+    length(AtomList,8),
+    maplist(atom_number(),AtomList,IntList),
     maplist(integer(),IntList),
     maplist(<(0) ,IntList),
     getMachine(0,Machine),
     assertArray(0,Machine,IntList).
 
 %Finds next unassigned machine (Check =< 8 is intentional, do not make it 7)
-getMachine(Check, Machine) :- 
+getMachine(Check, Machine) :-
     \+ treeNode(_,Check,_), Machine is Check;
     Check =< 8, NewCheck is Check+1, getMachine(NewCheck, Machine).
 
 %Asserts all elements in the list as new treeNodes
 assertArray(_,_,[]).
-assertArray(Task, Machine, [Penalty|Remaining]) :- 
+assertArray(Task, Machine, [Penalty|Remaining]) :-
     assert(treeNode(Task, Machine, Penalty)),
     NewTask is Task+1,
     assertArray(NewTask, Machine, Remaining).
 
+	
 
 
-%DCG for parsing Too-near Penalty information
-tnp-->tnpsHeader,tnpsBody,after.
 
-tnpsHeader--> ['too-near penalities'].
-tnpsBody-->[].
-
-
-arbLines --> [''],after.
-after-->[].
-after-->[''],after.
-
-hasInternalSpace(X):- 
+%Checks if name is of valid form (no spaces)
+hasInternalSpace(X):-
     atom_length(X,Y),
     Y>0,
     split_string(X,' ','',Z),
@@ -116,8 +272,6 @@ removeLastSpaces(AtomStart, Return) :- atom_chars(AtomStart, List), reverse(List
 removeSpaces(List, ListMinusSpaces) :- List = [H|T], H = '\s', removeSpaces(T, ListMinusSpaces).
 removeSpaces(List, List).
 
-
-%spaceCheck(X):-
 
 
 /*
